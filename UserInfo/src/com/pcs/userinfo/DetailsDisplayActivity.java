bc865eaaf192ba04f71659e41af4449a74193f91
AnithaGorli <anitha.gorli@paradigmcreatives>
2014-09-09 12:45:05 +0530

    change1

--- UserInfo/src/com/pcs/userinfo/DetailsDisplayActivity.java
+++ UserInfo/src/com/pcs/userinfo/DetailsDisplayActivity.java
@@ -1,7 +1,13 @@
 package com.pcs.userinfo;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class DetailsDisplayActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.display);
+	}
 
 }
--- UserInfo/src/com/pcs/userinfo/UserDetailsActivity.java
+++ UserInfo/src/com/pcs/userinfo/UserDetailsActivity.java
@@ -1,8 +1,15 @@
 package com.pcs.userinfo;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class UserDetailsActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	setContentView(R.layout.info);
+	}
 	
 
 }

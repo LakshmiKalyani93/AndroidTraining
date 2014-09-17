380c56151ddbf65c373b482441f2c2c1bd43fd5d
kalyani <kalyanikolimali93@gmail.com>
2014-09-17 11:42:26 +0530

    initial customdialog

---
+++ CustomDialog/gen/com/pcs/customdialog/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pcs.customdialog;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ CustomDialog/src/com/pcs/customdialog/SimpleDialogActivity.java
@@ -0,0 +1,5 @@
+package com.pcs.customdialog;
+
+public class SimpleDialogActivity {
+
+}

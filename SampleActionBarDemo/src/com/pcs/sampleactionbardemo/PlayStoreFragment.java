09b0d55f210ba4885e14834b40118d350a063831
kalyani <kalyanikolimali93@gmail.com>
2014-10-20 16:34:19 +0530

    ActionBars implemented with documentation

--- SampleActionBarDemo/src/com/pcs/sampleactionbardemo/PlayStoreFragment.java
+++ SampleActionBarDemo/src/com/pcs/sampleactionbardemo/PlayStoreFragment.java
@@ -1,18 +1,37 @@
 package com.pcs.sampleactionbardemo;
 
+import com.pcs.sampleactionbardemo.WebFragment.MyBrowser;
+
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class PlayStoreFragment extends Fragment{
+	private WebView webView;
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		//Inflating our own layout
 		View rootView = inflater.inflate(R.layout.playstore, container, false);
+		webView=(WebView)rootView.findViewById(R.id.web_View);
+		//Getting an instance to the class Browser
+		MyBrowser browse_googleplay=new MyBrowser();
+		webView.setWebViewClient(browse_googleplay);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.loadUrl("http://play.google.com/store");
+		
 		return rootView;
 
+	}
+	public static class MyBrowser extends WebViewClient{
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
 	}
 
 }
--- SampleActionBarDemo/src/com/pcs/sampleactionbardemo/WebFragment.java
+++ SampleActionBarDemo/src/com/pcs/sampleactionbardemo/WebFragment.java
@@ -1,19 +1,40 @@
 package com.pcs.sampleactionbardemo;
 
-import android.os.Bundle;
+
+import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class WebFragment extends Fragment{
+	private WebView webView;
+	@SuppressLint("SetJavaScriptEnabled")
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		//Inflating our own layout 
 		View rootView = inflater.inflate(R.layout.web, container, false);
+		//fetching webview of the layout web
+		webView=(WebView)rootView.findViewById(R.id.web_Vw);
+		//Getting an instance to the class Browser
+		MyBrowser browse_pcs=new MyBrowser();
+		webView.setWebViewClient(browse_pcs);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.loadUrl("http://paradigmcreatives.com");
+		
 		return rootView;
 
+	}
+	public static class MyBrowser extends WebViewClient{
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
 	}
 
 }

b1d0ca6b70b74de59d4c340ff26a9390897b2e7d
kalyani <kalyanikolimali93@gmail.com>
2014-10-22 15:23:12 +0530

    added few more testcases

---
+++ LoginDemoTest/src/com/pcs/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.pcs.constants;
+
+public class Constants {
+	
+	public interface KeyExtras{
+		
+		public static final String USERNAME = "kalyani";
+		public  static final String PASSWORD = "kalyani123";
+		public  static final String USER_INVALID="anitha";
+		public  static final String PWD_INVALID="anitha123";
+
+	}
+
+}
